a0bf54246634ca930b3470ac7213a5e4e0b490e0
gabrielasfc <gsfcunha@gmail.com>
2024-04-20 18:08:59 +0100

    new accelerometer data

--- app/src/main/java/com/example/elderwatch/SensorListener.kt
+++ app/src/main/java/com/example/elderwatch/SensorListener.kt
@@ -5,6 +5,9 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.util.Log
+import java.lang.Math.pow
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class SensorListener : SensorEventListener {
     companion object{
@@ -24,7 +27,11 @@ class SensorListener : SensorEventListener {
             AccelerometerData.valueZ = event.values[2]
             AccelerometerData.accuracy = event.accuracy
 
-            Log.d("Accelerometer", "[SENSOR] - X=${AccelerometerData.valueX}, Y=${AccelerometerData.valueY}, Z=${AccelerometerData.valueZ}")
+            //Log.d("Accelerometer", "[SENSOR] - X ${AccelerometerData.valueX}, Y ${AccelerometerData.valueY}, Z ${AccelerometerData.valueZ}")
+            val a: Double = AccelerometerData.valueX.toDouble().pow(2.toDouble())
+            val b: Double = AccelerometerData.valueY.toDouble().pow(2.toDouble())
+            val c: Double = (AccelerometerData.valueZ-9.8).toDouble().pow(2.toDouble())
+            Log.d("Accelerometer", "${sqrt(a+b+c)}")
         }
 
         if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
@@ -33,7 +40,7 @@ class SensorListener : SensorEventListener {
             GyroscopeData.valueZ = event.values[2]
             GyroscopeData.accuracy = event.accuracy
 
-            Log.d("Gyroscope", "[SENSOR] - X=${GyroscopeData.valueX}, Y=${GyroscopeData.valueY}, Z=${GyroscopeData.valueZ}")
+            //Log.d("Gyroscope", "[SENSOR] - X ${GyroscopeData.valueX}, Y ${GyroscopeData.valueY}, Z ${GyroscopeData.valueZ}")
         }
     }
 
