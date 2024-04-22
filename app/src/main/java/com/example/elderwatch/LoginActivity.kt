2ba165974cab305fb8dc650e021975a7563fb8cd
Guilherme Varela <guilherme.varelaaa@gmail.com>
2024-04-22 14:37:59 +0100

    mais coisas no login

--- app/src/main/java/com/example/elderwatch/LoginActivity.kt
+++ app/src/main/java/com/example/elderwatch/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.elderwatch
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -10,20 +11,23 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : ComponentActivity() {
-    private lateinit var auth: FirebaseAuth
+    //private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        auth = Firebase.auth
+        //auth = Firebase.auth
 
-        val loginButton = findViewById<Button>(R.id.loginButton)
+        val loginButton = findViewById<Button>(R.id.loginbtn)
+
         loginButton.setOnClickListener {
-            val email = findViewById<EditText>(R.id.email).text.toString()
+            val email = findViewById<EditText>(R.id.username).text.toString()
             val password = findViewById<EditText>(R.id.password).text.toString()
 
-            auth.signInWithEmailAndPassword(email, password)
+            Log.d("LOGIN", "${email} e ${password}")
+
+            /*auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Login sucesso, atualize a UI com as informações do usuário logado
@@ -34,7 +38,7 @@ class LoginActivity : ComponentActivity() {
                         Toast.makeText(baseContext, "Authentication failed.",
                             Toast.LENGTH_SHORT).show()
                     }
-                }
+                }*/
         }
     }
 }
