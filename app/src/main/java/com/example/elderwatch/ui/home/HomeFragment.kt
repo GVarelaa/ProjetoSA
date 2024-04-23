63f22aeca650e3354d97d19c6daec93a36f784fe
gabrielasfc <gsfcunha@gmail.com>
2024-04-23 16:05:01 +0100

    sos button on home page

--- app/src/main/java/com/example/elderwatch/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/elderwatch/ui/home/HomeFragment.kt
@@ -2,10 +2,13 @@ package com.example.elderwatch.ui.home
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.elderwatch.R
 
 class HomeFragment : Fragment() {
@@ -15,18 +18,35 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var viewModel: HomeViewModel
+    private lateinit var imageView: ImageView
+    private var isOn: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        imageView = view.findViewById(R.id.imageView)
+
+        imageView.setOnClickListener {
+            isOn = !isOn // Toggle the isOn state
+            imageView.setImageResource(if (isOn) R.drawable.sos_on else R.drawable.sos_off)
+
+            // If it's on, set it to off after 1 second
+            if (isOn) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    isOn = false
+                    imageView.setImageResource(R.drawable.sos_off)
+                }, 100) // 100 milliseconds delay for 1 second
+            }
+        }
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         // TODO: Use the ViewModel
     }
-
 }
