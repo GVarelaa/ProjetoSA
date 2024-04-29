083a252c989f9cb739a1e8f4eccaa25bee97d961
gabrielasfc <gsfcunha@gmail.com>
2024-04-29 11:44:50 +0100

    edit profile done

--- app/src/main/java/com/example/elderwatch/ui/settings/EditProfileFragment.kt
+++ app/src/main/java/com/example/elderwatch/ui/settings/EditProfileFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.elderwatch.R
 
 class EditProfileFragment : Fragment() {
@@ -22,11 +26,19 @@ class EditProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[EditProfileViewModel::class.java]
+
+        val backButton = view.findViewById<Button>(R.id.backButton)
         val emailEditText = view.findViewById<EditText>(R.id.email)
         val nameEditText = view.findViewById<EditText>(R.id.name)
         val phoneEditText = view.findViewById<EditText>(R.id.phone)
+        val saveButton = view.findViewById<Button>(R.id.savebtn)
 
         viewModel.userProfile.observe(viewLifecycleOwner) { data ->
             data["error"]?.let { error ->
@@ -41,7 +53,21 @@ class EditProfileFragment : Fragment() {
         // Make the email field non-editable
         emailEditText.isEnabled = false
 
-        return view
+        saveButton.setOnClickListener {
+            saveUserProfile(
+                nameEditText.text.toString(),
+                phoneEditText.text.toString()
+            )
+        }
+
+        backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_editProfileFragment_to_settingsFragment)
+        }
     }
 
+    private fun saveUserProfile(name: String, phone: String) {
+        viewModel.saveUserProfile(name, phone)
+        Toast.makeText(context, "Alterações efetuadas com sucesso!", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_editProfileFragment_to_settingsFragment)
+    }
 }
