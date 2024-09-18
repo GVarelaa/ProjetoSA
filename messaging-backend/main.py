fcda0a4a6fa8e6c0a0a4a1116ace2e1833c7d35f
Gabriela Cunha <73352341+gabrielasfc@users.noreply.github.com>
2024-09-18 16:43:44 +0100

    Update main.py

--- messaging-backend/main.py
+++ messaging-backend/main.py
@@ -33,4 +33,4 @@ def send_notification():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(debug=True)
